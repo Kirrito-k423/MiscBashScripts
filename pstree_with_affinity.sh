23fdad518cc4081b485c3e3efc3d30e89ba49c52
shaojieMike <943648187@qq.com>
2024-11-27 10:11:24 +0800

    Create pstree_with_affinity.sh

---
+++ pstree_with_affinity.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 检查是否提供了 PID
+if [ -z "$1" ]; then
+  echo "Usage: $0 <PID>"
+  exit 1
+fi
+
+# 获取 pstree 输出，带进程和线程信息
+pstree_output=$(pstree -t -a -p "$1")
+#echo $pstree_output
+# 输出进程树并为每个进程和线程打印亲和性
+echo "$pstree_output" | while read -r line; do
+  # 获取进程的 PID
+  pid=$(echo "$line" |awk -F',' '{print $2}' | tr -d ' '|sed -E 's/[^0-9]*([0-9]+)-.*/\1/')
+  #echo $pid
+  # 如果是进程，获取其亲和性
+  if [ -n "$pid" ]; then
+    affinity=$(taskset -cp "$pid" 2>/dev/null)
+    # 如果能获取到亲和性，输出
+    if [ -n "$affinity" ]; then
+      echo -e "$line \t\t Affinity: $affinity"
+    else
+      echo -e "$line \t\t Affinity: Unable to fetch"
+    fi
+  fi
+done
